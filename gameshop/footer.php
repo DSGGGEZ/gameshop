7be79375ba582161d38823cc682d5bea1f94b8da
DSGGGEZ <88538783+DSGGGEZ@users.noreply.github.com>
2022-03-02 10:45:17 +0700

    Delete gameshop directory

--- gameshop/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn->close();
-?>
-<hr>
-<p class="text-center small">Have fun with your game&nbsp;</p>
-<p class="text-center small"><?php print_r($_SESSION['member']);?></p>
-<p class="text-center small"><a href='logout.php'>Log Out</a></p>
-</body>
-</html>
--- gameshop/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include 'dbconnect.php';
-    session_start();
-
-if (isset($_SESSION['member'])) {
-    $sql = "SELECT * FROM member WHERE mid = '".$_SESSION['member']."'";
-	$query = $conn->query($sql);
-	$member = $query->fetch_assoc();
-}
